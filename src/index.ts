c74119e7098f39747d1fa7aa60260067deee49c5
Woojin Son <75523802+ichbinderwooj@users.noreply.github.com>
2023-01-08 20:26:13 +0100

    Added express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello world!');
+import express from 'express';
+
+const app = express();
+const PORT = 8002;
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
